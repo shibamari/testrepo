444fb7c26c170cd005f3261d3f4986e31083236e
shibamari <kk106816ms@gmail.com>
2021-03-25 01:34:00 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+index1      | items      | date
+------------ | ---------- | ----------
+tool         | phone       | 2021/03
+text         | books       | 2021/04
+tool         | pencil      | 2021/03
+textile      | skirts      | 2021/06
+
+
+index1      | items      | date
+------------ | ---------- | ----------
+tool         | phone       | 2021/03
+text         | books       | 2021/04
+tool         | pencil      | 2021/03
+textile      | skirts      | 2021/06
